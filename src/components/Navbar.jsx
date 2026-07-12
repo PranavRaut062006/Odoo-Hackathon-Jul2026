c32bec5bd11ce59e04afb5d59ec8918d8a7f1252
Rushikesh <rushirathod22082005@gmail.com>
2026-07-12 09:57:43 +0530

    Frontend Folder

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <>
+    <div>
+        <div>
+            <h3>Rushi.dev</h3>
+        </div>
+        <nav>
+  <ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+  </ul>
+</nav>
+    </div>
+    </>
+  )
+}
+
+
